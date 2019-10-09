dc04d9630504d4fc94dc6e947a7738992d0ba190
n.druzhinin <n.druzhinin@bestdoctor.ru>
2019-10-09 11:56:20 +0300

    rename var

--- main.py
+++ main.py
@@ -12,34 +12,33 @@ def download_image(image_url, folder_path):
 
 
 def fetch_spacex_last_launch():
-    url = "https://api.spacexdata.com/v3/launches/latest"
-    response = requests.get(url)
-    list_image_url = response.json()["links"]["flickr_images"]
+    spacex_api_url = "https://api.spacexdata.com/v3/launches/latest"
+    response = requests.get(spacex_api_url)
+    list_of_spacex_image_url = response.json()["links"]["flickr_images"]
+    for url_number, spacex_image_url in enumerate(list_of_spacex_image_url):
+        folder_path = '{}spacex{}.jpg'.format(images_directory, url_number + 1)
+        download_image(spacex_image_url, folder_path)
 
-    for url_number, image_url in enumerate(list_image_url):
-        folder_path = '{}spacex{}.jpg'.format(new_directory, url_number + 1)
-        download_image(image_url, folder_path)
 
-
-def download_hubble_image():
-    url = "http://hubblesite.org/api/v3/images/wallpaper"
-    response = requests.get(url)
-    list_id = []
-    for el in response.json():
-        list_id.append(el['id'])
-    for image_id in list_id:
-        url = "http://hubblesite.org/api/v3/image/{}".format(image_id)
-        response = requests.get(url)
-        list_of_hubble_url = response.json()['image_files']
-        hubble_url = list_of_hubble_url[-1]['file_url']
-        hubble_url = 'http:' + hubble_url
+def fetch_hubble_image():
+    hubble_collection_api_url = "http://hubblesite.org/api/v3/images/wallpaper"
+    response = requests.get(hubble_collection_api_url)
+    list_of_hubble_image_id = []
+    for hubble_image in response.json():
+        list_of_hubble_image_id.append(hubble_image['id'])
+    for image_id in list_of_hubble_image_id:
+        hubble_image_api_url = "http://hubblesite.org/api/v3/image/{}".format(image_id)
+        response = requests.get(hubble_image_api_url)
+        list_of_hubble_image_url = response.json()['image_files']
+        hubble_url = 'http:' + list_of_hubble_image_url[-1]['file_url']
         image_type = hubble_url.split('.')[-1]
-        folder_path = '{}{}.{}'.format(new_directory, image_id, image_type)
+        folder_path = '{}{}.{}'.format(images_directory, image_id, image_type)
         download_image(hubble_url, folder_path)
 
 
-new_directory = '/dvmn2/push_to_inst/images/'
-pathlib.Path(new_directory).mkdir(parents=True, exist_ok=True)
-urllib3.disable_warnings()
-fetch_spacex_last_launch()
-download_hubble_image()
+if __name__ == "__main__":
+    images_directory = '/dvmn2/push_to_inst/images/'
+    pathlib.Path(images_directory).mkdir(parents=True, exist_ok=True)
+    urllib3.disable_warnings()
+    fetch_spacex_last_launch()
+    fetch_hubble_image()
